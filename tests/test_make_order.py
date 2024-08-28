e2ffe64762257ecdb18e6d7b56374198153d07ef
Ivan Generalov <ivangeneralov@gmail.com>
2024-08-28 15:36:00 +0300

    fivth commit

--- tests/test_make_order.py
+++ tests/test_make_order.py
@@ -9,7 +9,7 @@ class TestCreationOrder:
 
     @allure.title('Successful order creation with all color variations. Handle:/api/v1/orders')
     @pytest.mark.parametrize('scooter_color', DataForOrder.scooter_color)
-    def test_creation_order_choice_color_true(self, scooter_color):
+    def test_create_order_with_diff_colors(self, scooter_color):
         order_data = DataForOrder.order_data
         order_data['color'] = scooter_color
         order = requests.post(f'{Url.MAIN_URL}{Url.CREATE_ORDER}', json=order_data)
